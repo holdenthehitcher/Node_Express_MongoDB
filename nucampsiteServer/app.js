971cf56ae81da381fec035edb2f3835825172939
Holden Nakamura <holdenthehitcher@gmail.com>
2020-08-14 23:30:14 -1000

    setup pathway for comments

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -23,7 +23,7 @@ const connect = mongoose.connect(url, {
 connect.then(() => console.log('Connected correctly to server'), 
     err => console.log(err)
 );
-
+ 
 var app = express();
 
 // view engine setup
