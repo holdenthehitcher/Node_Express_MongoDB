41fe9b777920b105917d679c34cada88663c0f10
Holden Nakamura <holdenthehitcher@gmail.com>
2020-08-27 12:01:02 -1000

    redirected to sercure https server

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 const passport = require("passport");
-const config = require('./config');
+const config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -28,6 +28,16 @@ connect.then(
 
 var app = express();
 
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    console.log(`Redirecting to: https://${req.hostname}:${app.get("secPort")}${req.url}`);
+    res.redirect(301, `https://${req.hostname}:${app.get("secPort")}${req.url}`);
+  }
+});
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
@@ -36,7 +46,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
-
 
 app.use(passport.initialize());
 
