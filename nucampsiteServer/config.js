a564e1edbb1d9e5ad09d4ff49803a889543a47ed
Holden Nakamura <holdenthehitcher@gmail.com>
2020-08-27 19:47:46 -1000

    facebook auth

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "348885799627194",
+    clientSecret: "a88259bcdd381c7cf83a2cd5ef881a49",
+  },
 };
